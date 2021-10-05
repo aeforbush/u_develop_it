fd2dd3378d7628d0e8cc3c78705de54b2b485882
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-10-05 13:17:09 -0600

    added mysql info

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2')
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,19 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// connection to mysql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // mysql username
+        user: 'root',
+        // mysql password
+        password: 'SRMa204@?ZC',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 // default response for any other request *needs to be at bottom (Not Found)
