1f681ca97ee2b740dd1b55c691cc061361d33900
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-10-07 13:40:02 -0600

    modularized party routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const inputCheck = require("./utils/inputCheck");
-const db = require('./db/connection');
-const apiRoutes = require('./routes/apiRoutes');
+// const inputCheck = require("./utils/inputCheck");
+const db = require("./db/connection");
+const apiRoutes = require("./routes/apiRoutes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,72 +9,20 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/api', apiRoutes);
 
-
-
-app.get("/api/parties", (req, res) => {
-    const sql = `SELECT * FROM parties`;
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: "success",
-        data: rows,
-      });
-    });
-  });
-  
-  app.get("/api/party/:id", (req, res) => {
-    const sql = `SELECT * FROM parties WHERE id = ?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, row) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: "success",
-        data: row,
-      });
-    });
-  });
-
-
-
-
-app.delete("/api/party/:id", (req, res) => {
-  const sql = `DELETE FROM parties WHERE id = ?`;
-  const params = [req.params.id];
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: res.message });
-      // checks if anything was deleted
-    } else if (!result.affectRows) {
-      res.json({
-        message: "Party not found",
-      });
-    } else {
-      res.json({
-        message: "deleted",
-        changes: result.affectedRows,
-        id: req.params.id,
-      });
-    }
-  });
-});
-
-
-
-
+// use apiRoutes
+app.use("/api", apiRoutes);
 
 // default response for any other request *needs to be at bottom (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// start server after DB connection
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Database conected.");
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
